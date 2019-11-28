50296e21730c90beaaa47415b932251c610ad73f
John Aspinall <johnaspinall@rentalcars.com>
2019-11-28 17:57:13 +0000

    Commented the HTML linting task out due to bug

--- gruntfile.js
+++ gruntfile.js
@@ -136,7 +136,7 @@ module.exports = function (grunt) {
                 'sass',
                 'cssmin',
                 'imagemin',
-                'htmlhint',
+               // 'htmlhint',
                 'concat',
                 'copy',
                 'connect',
